322ff01efdc5454ef9976f2e52da12866677bcf1
SNEHA CHAKRABORTY <snehachakraborty0229@gmail.com>
2026-02-15 02:16:53 +0530

    add formula based conversion

---
+++ basics/area_circle.py
@@ -0,0 +1,3 @@
+radius = float(input("enter radius value: "))
+area = 3.14*(radius**2)
+print("area of circle = ",area)
---
+++ basics/bmi_calculator.py
@@ -0,0 +1,4 @@
+h = float(input("enter height (in m): "))
+w = float(input("enter weight (in kg): "))
+bmi = w/(h**2)
+print("BMI value = ",bmi)
