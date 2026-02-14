28c2329c4c965af55224c73a67334efb351a7579
SNEHA CHAKRABORTY <snehachakraborty0229@gmail.com>
2026-02-15 02:14:29 +0530

    add datatype conversion

---
+++ basics/check_type.py
@@ -0,0 +1,3 @@
+s = input("enter element: ")   #string input
+print(type(s))                 #prints class type of element
+print("after conversion:\n",type(int(s)))
---
+++ basics/conversion_string.py
@@ -0,0 +1,4 @@
+s1=input("enter element 1: ")
+s2=input("enter element 2: ")
+sum1= int(s1)+int(s2)
+print("summation= ",sum1)
