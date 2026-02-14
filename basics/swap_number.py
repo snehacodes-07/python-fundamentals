2b0dfdcf112a685a32fba216668a3d695d44507c
SNEHA CHAKRABORTY <snehachakraborty0229@gmail.com>
2026-02-15 02:17:37 +0530

    add number swapping

---
+++ basics/swap_number.py
@@ -0,0 +1,7 @@
+num1 = int(input("enter 1st element: "))
+num2 = int(input("enter 2nd element: "))
+temp=num1
+num1=num2
+num2=temp
+print("before: \nfirst element =",temp, "\nsecond element =",num1)
+print("before: \nfirst element =",num1, "\nsecond element =",num2)
