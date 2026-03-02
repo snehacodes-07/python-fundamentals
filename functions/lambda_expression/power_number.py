5b2f57548bc3d9acf4a9ef911d566cac1ac29a4f
SNEHA CHAKRABORTY <snehachakraborty0229@gmail.com>
2026-03-02 05:53:14 +0530

    add exponential calculation

---
+++ functions/lambda_expression/power_number.py
@@ -0,0 +1,5 @@
+a = int(input("enter number: "))
+b = int(input("enter power: "))
+s = lambda x,y: x**y
+print("calculating...")
+print("result: ",s(a,b))
---
+++ functions/lambda_expression/square_number.py
@@ -0,0 +1,4 @@
+a = int(input("enter number: "))
+s = lambda x: x**2
+print("calculating...")
+print("result: ",s(a))
