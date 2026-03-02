61a8bde021bbcfb8ab193fd00aeea368a617d7db
SNEHA CHAKRABORTY <snehachakraborty0229@gmail.com>
2026-03-02 05:54:11 +0530

    add sum of list elements

---
+++ functions/lambda_expression/sum_list_elements.py
@@ -0,0 +1,14 @@
+def lst():
+    size = int(input("enter size of list: "))
+    l = []
+    for i in range(1,size+1):
+        print("enter element",i,": ",end='')
+        a = int(input())
+        l.append(a)
+    return(l)
+
+arr = lst()
+print("given list: ",arr)
+
+summation = sum(list(map(lambda x: x, arr)))
+print("summation of elements: ",summation)
