cb33a8bfae02be970c2e94f8a2f14c473027e9ae
SNEHA CHAKRABORTY <snehachakraborty0229@gmail.com>
2026-03-02 05:46:37 +0530

    add exponential value calculation

---
+++ functions/user_defined/power_number.py
@@ -0,0 +1,7 @@
+def power(a,b):
+    print("calculating....")
+    return(a**b)
+a = int(input("enter number: "))
+b = int(input("enter power: "))
+powt = power(a,b)
+print("result: ",powt)
---
+++ functions/user_defined/square_number.py
@@ -0,0 +1,6 @@
+def square(a):
+    print("calculating....")
+    return(a**2)
+a = int(input("enter number: "))
+s = square(a)
+print("result: ",s)
