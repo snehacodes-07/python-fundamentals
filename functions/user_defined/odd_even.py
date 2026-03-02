38d2a1ed90007984106f2e9a476de096cfc7151f
SNEHA CHAKRABORTY <snehachakraborty0229@gmail.com>
2026-03-02 05:45:49 +0530

    add odd even verification

---
+++ functions/user_defined/odd_even.py
@@ -0,0 +1,7 @@
+def check(a):
+    if a%2 == 0:
+        print("the number is divisible by 2 \n\nIT'S AN EVEN NUMBER")
+    else:
+        print("the number is not divisible by 2 \n\nIT'S AN ODD NUMBER")
+a = int(input("enter number: "))
+check(a)
